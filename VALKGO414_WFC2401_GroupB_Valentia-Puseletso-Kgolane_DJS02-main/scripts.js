8d807af471997da82a15c0cc94b487169dcdb948
Valentia Puseletso Kgolane <156904482+VPKGO414@users.noreply.github.com>
2024-05-10 16:48:14 +0200

    Update scripts.js
    Modifications have been made

--- VALKGO414_WFC2401_GroupB_Valentia-Puseletso-Kgolane_DJS02-main/scripts.js
+++ VALKGO414_WFC2401_GroupB_Valentia-Puseletso-Kgolane_DJS02-main/scripts.js
@@ -5,5 +5,37 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  // Convert inputs to numbers
+  const dividendNumber = parseFloat(dividend);
+  const dividerNumber = parseFloat(divider);
+
+  // Check if inputs are valid numbers
+  if (isNaN(dividendNumber) || isNan(dividerNumber)) {
+    result.innerHTML = "Division not performed. Invalid number provided. Try again";
+    return;
+  }
+
+  // Check if divider is zero
+  if (dividerNumber === 0) {
+    result.innerHTML = "Division not performed. Cannot divide by zero.";
+    console.error("Division by zero attempted.");
+    return;
+  }
+
+  // Check if both inputs are filled
+  if (!dividend.trim() || !divider.trim()) {
+    result.innerHTML = "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
+
+  //perform division
+  const divisionResult = dividendNumber / dividerNumber;
+
+  // Check if the result is a whole nuumber
+  if (divisionResult % 1 === 0) {
+    result.innerText = divisionResult;
+  } else{
+    result.innerText = Math.trunc(divisionResult); // Display whole number without decimal places
+  }
 });
